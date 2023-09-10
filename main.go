e11ff140b76769a7e7a2dbf260fe894383fd0175
Su-pr007 <maksimka.suvorov.01@list.ru>
2023-09-11 00:02:29 +0300

    env file added

--- main.go
+++ main.go
@@ -1,18 +1,34 @@
 package main
 
 import (
+	"os"
+	"log"
 	"html/template"
 	"net/http"
+	"github.com/joho/godotenv"
 )
 
 type PageData struct {
 	Title    string
 }
 
+// init is invoked before main()
+func init() {
+	if err := godotenv.Load(); err != nil {
+		log.Print("No .env file found")
+	}
+}
+
 func main() {
+	appPort, exists := os.LookupEnv("APP_PORT")
+
+	if !exists {
+		log.Print("Port is not set")
+	}
+
 	http.HandleFunc("/", handler)
 	http.HandleFunc("/test", testHandler)
-	http.ListenAndServe(":4000", nil)
+	http.ListenAndServe(":"+appPort, nil)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
